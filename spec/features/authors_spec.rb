700f2bdc9ed03f94b212617b61fcd916648bac53
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-01 22:51:54 +0100

    Extend failing test for #2

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 describe 'authors page' do
 
-  it 'should render the page' do
+  it 'should render the page with the correct input fields' do
     visit 'authors/new'
+    expect(page).to have_field('First name')
+    expect(page).to have_field('Last name')
+    expect(page).to have_field('Homepage')
   end
 end
