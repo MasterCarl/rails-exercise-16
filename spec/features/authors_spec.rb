bd7ff6f623259bd471f4ffed2ad0e9c194a47e4a
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-03 15:42:18 +0100

    Failing test for #4

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 describe 'authors page' do
 
-  it 'should render the page with the correct input fields' do
+  it 'should render the page with the correct input fields and buttons' do
     visit 'authors/new'
     expect(page).to have_field('First name')
     expect(page).to have_field('Last name')
     expect(page).to have_field('Homepage')
+    expect(page).to have_button('Create Author')
   end
 end
