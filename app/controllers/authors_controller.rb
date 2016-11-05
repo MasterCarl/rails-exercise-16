f424cf3e9104c8a928210d9451ce3c82057d266d
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-05 23:46:49 +0100

    Add authors controller functionality. Should close #6 (tests not included right now).

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,10 @@ class AuthorsController < ApplicationController
   def new
     
   end
+
+  def create
+    @author = Author.new(params[:author])
+    @author.save
+    redirect_to @author
+  end
 end
