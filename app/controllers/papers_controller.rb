6135d936fdc76d6679a0ab2ce280eedae29f4efc
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-12 21:53:23 +0100

    Copy papers controller logic and templates from authors

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -11,4 +11,38 @@ class PapersController < ApplicationController
       render 'new'
     end
   end
+
+  def show
+    @paper = Paper.find(params[:id])
+  end
+
+  def edit
+    @paper = Paper.find(params[:id])
+  end
+
+  def index
+    @papers = Paper.all
+  end
+
+  def update
+    @paper = Paper.find(params[:id])
+
+    if @paper.update(paper_params)
+      redirect_to @paper
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @paper = Paper.find(params[:id])
+    @paper.destroy
+
+    redirect_to authors_path
+  end
+
+  private
+  def paper_params
+    params.require(:paper).permit(:title, :venue, :year)
+  end
 end
