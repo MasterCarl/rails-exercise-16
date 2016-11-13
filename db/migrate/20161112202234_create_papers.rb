e2b7dd5916a3b2902c92a17e81583d4b97cb088c
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-13 15:29:03 +0100

    Move index creation to create_join_table, revert b43e88eaa2ec956922df7681e183bb6c96a8fde1

--- db/migrate/20161112202234_create_papers.rb
+++ db/migrate/20161112202234_create_papers.rb
@@ -4,10 +4,9 @@ class CreatePapers < ActiveRecord::Migration
       t.string :title
       t.string :venue
       t.integer :year
-      t.integer  "authors_id"
       t.timestamps null: false
     end
 
-    add_index "papers", ["authors_id"], name: "index_papers_on_authors_id"
+    #add_index "papers", ["authors_id"], name: "index_papers_on_authors_id"
   end
 end
