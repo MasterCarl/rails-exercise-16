837a48d4998ad5f0d55625322354cbb8a8d9d739
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-13 23:08:20 +0100

    Refactor papers_spec

--- spec/features/papers_spec.rb
+++ spec/features/papers_spec.rb
@@ -24,13 +24,13 @@ end
 describe 'Paper editing page' do
   before(:each) do
     @paper = FactoryGirl.build(:paper)
+    @paper.save()
   end
   it 'should render the page' do
-    @paper.save()
     visit edit_paper_path(@paper)
   end
+
   it 'should allow to select 5 authors from 5 separate drop downs' do
-    @paper.save()
     visit edit_paper_path(@paper)
     (1..5).each do |number|
       expect(page).to have_select("paper_author_id_#{ number }")
