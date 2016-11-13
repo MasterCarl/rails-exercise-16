e93c4a6c2103dda0a03906c1979a2d9f49efe994
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-11-13 11:33:15 +0100

    Failing tests for #33

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @author = FactoryGirl.build(:author)
+  end
+  it "should have an empty list of papers by default" do
+    expect(@author.papers).to be_empty
+  end
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  it "shouldn't save if no year is given" do
+
+  before(:each) do
     @paper = FactoryGirl.build(:paper)
+  end
+  it "shouldn't save if no year is given" do
     @paper.year = nil
     expect { @paper.save! }.to raise_error ActiveRecord::RecordInvalid
   end
 
   it "shouldn't save if no year is given" do
-    @paper = FactoryGirl.build(:paper)
     @paper.year = "This Year"
     expect { @paper.save! }.to raise_error ActiveRecord::RecordInvalid
+  end
+
+  it "should have an empty list of authors by default" do
+    expect(@paper.authors).to be_empty
   end
 end
